71930bb8c8f528c298f247fa7361262857347b04
Alfin Reynaldi <alfin.reynaldi26@gmail.com>
2026-08-28 10:40:27 +0700

    feat(next.config): add permanent redirects for all incoming paths
    redirect all incoming requests to https://alfinry.vercel.app/ using permanent 301 redirects

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://alfinry.vercel.app/",
+        permanent: true,
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
